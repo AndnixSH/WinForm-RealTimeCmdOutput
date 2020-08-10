78de0a589ae93ac457e754929e59b63ad2069adc
AndnixSH <40742924+AndnixSH@users.noreply.github.com>
2020-08-10 16:48:03 +0200

    Add files via upload

---
+++ RealTimeCmdOutput/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RealTimeCmdOutput
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runBtn = new System.Windows.Forms.Button();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // runBtn
+            // 
+            this.runBtn.Location = new System.Drawing.Point(190, 12);
+            this.runBtn.Name = "runBtn";
+            this.runBtn.Size = new System.Drawing.Size(75, 23);
+            this.runBtn.TabIndex = 0;
+            this.runBtn.Text = "Run";
+            this.runBtn.UseVisualStyleBackColor = true;
+            this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Location = new System.Drawing.Point(12, 63);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.Size = new System.Drawing.Size(432, 130);
+            this.richTextBoxOutput.TabIndex = 1;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 209);
+            this.Controls.Add(this.richTextBoxOutput);
+            this.Controls.Add(this.runBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runBtn;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+    }
+}
+
---
+++ RealTimeCmdOutput/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealTimeCmdOutput
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void runBtn_Click(object sender, EventArgs e)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                startProgram("ping.exe", "8.8.8.8");
+                TxtBoxOutput("Done :)");
+            });
+        }
+
+        private void startProgram(string filename, string commandLine)
+        {
+            var fileName = filename;
+            var arguments = commandLine;
+
+            var info = new ProcessStartInfo();
+            info.FileName = fileName;
+            info.Arguments = arguments;
+
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.CreateNoWindow = true;
+
+            using (var p = new Process())
+            {
+                p.StartInfo = info;
+                p.EnableRaisingEvents = true;
+
+                p.OutputDataReceived += (s, o) =>
+                {
+                    TxtBoxOutput(o.Data);
+                };
+                p.ErrorDataReceived += (s, o) =>
+                {
+                    TxtBoxOutput(o.Data);
+                };
+                p.Start();
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+                p.WaitForExit();
+            }
+        }
+
+        public void TxtBoxOutput(string text)
+        {
+            BeginInvoke(new Action(delegate ()
+            {
+                richTextBoxOutput.AppendText(text + Environment.NewLine);
+                richTextBoxOutput.ScrollToCaret();
+            }));
+        }
+    }
+}
